8b3f5996f4e6cd144dd3240a49faaa28bf130f26
Gene Kaufman <genekaufman@gmail.com>
2016-04-21 17:04:02 -0400

    started ex2

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -4,13 +4,18 @@ function g = sigmoid(z)
 
 % You need to return the following variables correctly 
 g = zeros(size(z));
-
+e = exp(1);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
+numCols = size(z,2);
+numRows = size(z,1);
 
-
-
+for col = 1:numCols
+    for row= 1:numRows
+        g(row,col) = 1 / (1 + e .^ (-1 * z));
+    end 
+end
 
 
 % =============================================================
