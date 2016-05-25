721d2a257bf4fec5223e1b6e550bc53cc5b0353b
Gene Kaufman <genekaufman@gmail.com>
2016-05-25 12:43:21 -0400

    ex7 finished

--- ex7/projectData.m
+++ ex7/projectData.m
@@ -8,14 +8,17 @@ function Z = projectData(X, U, K)
 
 % You need to return the following variables correctly.
 Z = zeros(size(X, 1), K);
-
+%size(U)
+%size(X)
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
 %               eigenvectors in U (first K columns). 
 %               For the i-th example X(i,:), the projection on to the k-th 
 %               eigenvector is given as follows:
-                    x = X(1:K, :)';
-                    Z = x' * U(:,1:K);
+%                    x = X(1:K, :)';
+
+                    Z = X * U(:,1:K);
+%                    Z = x' * U;
 %                    projection_k = x' * U(:, k);
 %
 
--- ex7/recoverData.m
+++ ex7/recoverData.m
@@ -20,7 +20,13 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %
 %               Notice that U(j, 1:K) is a row vector.
 %               
+thisU = U(:,1:K);
+%size(Z)
+%size(thisU')
+%size(X_rec)
 
+
+X_rec = Z * thisU';
 
 
 % =============================================================
